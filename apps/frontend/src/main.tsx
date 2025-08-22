90bb61416474095840c7dcf8917e4083f03f9090
root <root@srv531342.hstgr.cloud>
2025-08-22 01:12:04 +0000

    arreglos previos para puesta en marcha

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -19,7 +19,7 @@ createRoot(document.getElementById("root")!).render(
           domain={import.meta.env.VITE_AUTH0_DOMAIN}
           clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
           authorizationParams={{
-            redirect_uri: `${import.meta.env.VITE_REDIRECT_URI}`,
+            redirect_uri: `${window.location.origin}`,
 
             audience: import.meta.env.VITE_AUTH0_AUDIENCE,
           }}
